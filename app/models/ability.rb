c2db9f7f8efb301e4b38191376065b153b450c4f
bsearle89 <br.searle89@gmail.com>
2017-11-30 22:09:59 +0000

    6.8 install brakeman, conduct security check

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,8 @@ class Ability
     if user.admin?
     	can :manage, :all
     else
-    	can %i[show update], User, id: user.id
-    	can %i[show index], Order, user_id: user.id
-    	can %i[destroy create], OrderProduct, order_id: user.active_order.id
-    	can :create, Comment, user_id: user.id
-    	can :show, Comment
+    	can :read, :all
+      can :manage, User, id: user.id
     end
   end
   
